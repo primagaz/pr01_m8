c7aba57005c3bbc6904c1572282b2c08ca123513
adaw <adaw@localhost>
2017-10-05 15:23:31 +0200

    Versio 0.3

--- pr02_m8/src/pr02_m8/Pr02_m8.java
+++ pr02_m8/src/pr02_m8/Pr02_m8.java
@@ -9,13 +9,29 @@ package pr02_m8;
  *
  * @author adaw
  */
+import java.io.*;
 public class Pr02_m8 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write(" <head>");bw.newLine();
+            bw.write(" <title>");bw.newLine();
+            bw.write(" Nova p&agrave;gina web");
+           bw.newLine();
+           bw.write(" </title>");
+           bw.newLine();
+            bw.write(" </head>");
+           bw.newLine();
+            bw.write(" <body>");
+           bw.newLine();
+           bw.write(" Nova p&agrave;gina web");
+           bw.newLine();
+           bw.write(" </body>");
+           bw.newLine();
+           bw.write("</html>");
+           bw.newLine();
+           bw.close();
+        }
     }
-    
 }
